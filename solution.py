54991c0eecba9da1d629aabff3bb3f3856f0938b
aleksalm109 <71154045+aleksalm109@users.noreply.github.com>
2023-04-13 22:33:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 881258336 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p = st.mannwhitneyu(x, y, alternative='two-sided').pvalue/2
+    if p < 0.03:
+        return True
+    else:
+        return False
