1192d375b3573db9ff90f1e03058127ffd0fb74d
aleksalm109 <71154045+aleksalm109@users.noreply.github.com>
2023-04-13 23:51:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,6 @@ import scipy.stats as st
 chat_id = 881258336 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p = st.mannwhitneyu(x, y, alternative='two-sided').pvalue/2
-    if p < 0.03:
-        return True
-    else:
-        return False
+    alpha = 0.04
+    a = st.anderson_ksamp([x, y]).significance_level
+    return a < alpha
